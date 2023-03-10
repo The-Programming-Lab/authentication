03f413165541e4d91ee4d5f5a0570c879369823a
BraedenHCL <braeden.norman6@gmail.com>
2023-03-09 23:00:20 -0800

    add workflow to deploy to gke

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 ## How to run
+Put `.env` and `.env.dev` file in the root directory
 
-### Build
-`docker build -t auth .`
-### Run
-`docker run -p 80:80 auth`
-### Access
-`http://localhost:80/docs`
+## Production
+Build: `docker build -t auth .`<br>
+Run: `docker run -p 80:80 auth`<br>
+Access: `http://localhost:80/docs`
+
+
+## Development
+Build: `docker build -f Dockerfile.dev -t dev-auth .`<br>
+Run: `docker run -p 80:80 dev-auth`<br>
+Access: `http://localhost:80/docs`<br>
+### or without docker
+virtualenv setup: https://www.youtube.com/watch?v=IAvAlS0CuxI<br>
+Install requirements: `pip install -r requirements.txt`<br>
+Run: `python ./app/main.py`<br>
+Access: `http://localhost:8000/docs`<br>
+
 
 
 ### links 
--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-load_dotenv()
+
+if os.getenv('ENVIRONMENT') == 'production':
+    load_dotenv('.env')
+elif os.getenv('ENVIRONMENT') == 'development':
+    load_dotenv('.env.dev')
+
 
 firebaseConfig = {
     "apiKey": os.getenv('API_KEY'),
@@ -15,12 +20,12 @@ firebaseConfig = {
     "messagingSenderId":  os.getenv('MESSAGING_SENDER_ID'),
     "appId":  os.getenv('APP_ID'),
     "measurementId":  os.getenv('MEASUREMENT_ID'),
-    "databaseURL":  os.getenv('DATABASE_URL')
+    "databaseURL":  os.getenv('DATABASE_URL'),
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
 
+auth = firebase.auth()
 
 app = FastAPI()
 
@@ -39,13 +44,13 @@ class User(BaseModel):
     email: str
     password: str
 
-
 @app.post("/register")
 async def register(newUser : User):
     try:
         auth.create_user_with_email_and_password(newUser.email, newUser.password)
         return {"message": "User created successfully"}
-    except:
+    except Exception as error:
+        print(error)
         return {"message": "User already exists"}
     
 
@@ -57,3 +62,12 @@ async def login(user : User):
     except:
         return {"message": "Invalid credentials"}
 
+@app.post("/test")
+async def test():
+    return {"message": "Test successful"}
+
+if __name__ == "__main__":
+    import uvicorn
+    load_dotenv('.env.dev')
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    
